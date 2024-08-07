aa0d971aeb3d0692900c481eb7b673ff8b935347
Mhd Azzawi <le_prince_d.amour@hotmail.com>
2024-08-07 08:45:25 -0800

    add strings and colors

---
+++ lib/design/color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0x0008b882);
+  static const Color secondaryColor = Color(0xFFFFFFFF);
+  static const Color accentColor = Color(0x00008955);
+  static const Color infoColor = Color(0x00B8B8B8);
+  static const Color basecolor = Color(0x00bae6d1);
+  static const Color primary300 = Color(0x008cbaba);
+  static const Color warningColor300 = Color(0x00fabf2e);
+  static const Color warningyellow900 = Color(0x00f58017);
+  static const Color errorred700 = Color(0x00d42e2e);
+}
