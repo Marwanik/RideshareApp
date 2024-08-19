abfed919923de24c2464f7e628e6022aa0407324
unknown <marwan.kawadri.2000@gmail.com>
2024-08-19 21:59:43 +0300

    Get all Hubs

--- lib/bloc/Login/AuthBlocLogin.dart
+++ lib/bloc/Login/AuthBlocLogin.dart
@@ -3,25 +3,43 @@ import 'package:meta/meta.dart';
 import 'package:rideshare/bloc/Login/AuthEventLogin.dart';
 import 'package:rideshare/bloc/Login/AuthStateLogin.dart';
 import 'package:rideshare/repos/AuthRepoLogin.dart';
-import 'package:rideshare/model/loginModel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthBlocLogin extends Bloc<AuthEventLogin, AuthStateLogin> {
   final AuthRepoLogin authRepo;
-  String? _authToken; // Private token variable
+  String? _authToken;
 
   AuthBlocLogin(this.authRepo) : super(AuthInitialLogin()) {
     on<Login>((event, emit) async {
       emit(LoadingLogin());
       try {
         final token = await authRepo.login(event.user);
-        _authToken = token;  // Store the token
-        print('Auth token received: $_authToken');  // Print the token
+        _authToken = token;
+        await _saveToken(token);  // Save the token to shared preferences
         emit(SuccessLogin(token));
       } catch (e) {
         emit(FailedLogin(message: e.toString()));
       }
     });
+
+    _loadToken();  // Load the token when the Bloc is initialized
   }
 
-  String? get authToken => _authToken;  // Expose the token through a getter
+  String? get authToken => _authToken;
+
+  Future<void> _saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('authToken', token);
+  }
+
+  Future<void> _loadToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    _authToken = prefs.getString('authToken');
+  }
+
+  Future<void> clearToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('authToken');
+    _authToken = null;
+  }
 }
